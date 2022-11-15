8a2f5f80d2fddc349ee1a75e4be2d333faa88a9a
Steve Onyeneke <bobsarglobal@gmail.com>
2022-11-15 18:38:44 +0000

    implement CreateUser functionality

--- pkg/user/user.go
+++ pkg/user/user.go
@@ -1,17 +1,27 @@
 package user
 
 import (
+	"encoding/json"
 	"errors"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"github.com/bobsar0/go-aws-serverless/pkg/validators"
 )
 
 var (
 	ErrorFailedToFetchRecord     = "Failed to fetch record"
 	ErrorFailedToUnmarshalRecord = "Failed to unmarshal record"
+	ErrorInvalidUserData = "Invalid user data"
+	ErrorInvalidEmail = "Invalid email"
+	ErrorCouldNotMarshalItem = "Could not marshal item"
+	ErrorCouldNotDeleteItem = "Could not delete item"
+	ErrorDynamoCouldNotPutItem = "Dynamo could not put items into"
+	ErrorUserAlreadyExists = "User already exists"
+	ErrorUserDoesNotExist = "User does not exist"
 )
 
 type User struct {
@@ -63,11 +73,44 @@ func FetchUsers(tableName string, dynamoClient dynamodbiface.DynamoDBAPI) (*[]Us
 
 }
 
-func CreateUser() {
+func CreateUser(req events.APIGatewayProxyRequest, tableName string, dynamoClient dynamodbiface.DynamoDBAPI) (
+	*User, error,
+	) {
+		var user User
 
+		if err := json.Unmarshal([]byte(req.Body), &user); err != nil {
+			return nil, errors.New(ErrorInvalidUserData)
+		}
+
+		if !validators.IsEmailValid(user.Email) {
+			return nil, errors.New(ErrorInvalidEmail)
+		}
+
+		currUser, _ := FetchUser(user.Email, tableName, dynamoClient)
+
+		if currUser != nil && len(currUser.Email) != 0 {
+			return nil, errors.New(ErrorUserAlreadyExists)
+		}
+
+		av, err := dynamodbattribute.MarshalMap(user)
+		if err != nil {
+			return nil, errors.New(ErrorCouldNotMarshalItem)
+		}
+
+		input:= &dynamodb.PutItemInput{
+			Item: av,
+			TableName: aws.String(tableName),
+		}
+
+		_, err = dynamoClient.PutItem(input)
+		if err != nil {
+			return nil, errors.New(ErrorDynamoCouldNotPutItem)
+		}
+
+		return &user, nil
 }
 
-func UpdateUser() {
+func UpdateUser(email string, tableName string, dynamoClient dynamodbiface.DynamoDBAPI) {
 
 }
 
